f9fb0476796b02bf948c780aebc13c772d58abb5
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-05 15:51:45 +0000

    MVP: Worked on card component and added more pictures to the grid

--- components/Cards.js
+++ components/Cards.js
@@ -1,9 +1,12 @@
-const cardContainer = document.querySelector('.cards-container');
+
+console.log('dom content loaded');
+const cardsContainer = document.querySelector('.cards-container');
 const overlay = document.querySelector('.overlay');
 const overlayImage = overlay.querySelector('img');
 const overlayClose = overlay.querySelector('.close');
 
 function showOverlay(event) {
+  console.log('clicked');
   const src = event.currentTarget.querySelector('img').src;
   overlayImage.src = src;
   overlay.classList.add('open');
@@ -14,7 +17,10 @@ function close() {
 }
 
 const cards = document.querySelectorAll('.card');
+console.log(cards);
 
 cards.forEach(card => card.addEventListener('click', showOverlay));
 
 overlayClose.addEventListener('click', close);
+console.log('hello');
+console.log('loaded Cards.js');
